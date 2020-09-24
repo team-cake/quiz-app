faec471d45b70a6d7cb02243070ed5d5cdd8bcbf
team-cake <eka.cheuk@gmail.com>
2020-09-24 18:22:51 +0200

    Set up initial foundation for the quiz

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import React from 'react'
 
 function App() {
-	return <div className='App'>quiz</div>
+	const startTrivia = async () => {}
+
+	const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {}
+
+	const nextQuestion = () => {}
+
+	return (
+		<div className='App'>
+			<h1>Trivia Quiz</h1>
+			<button className='start' onClick={startTrivia}>
+				Start
+			</button>
+			<p className='score'>Score:</p>
+			<p>Loading questions...</p>
+		</div>
+	)
 }
 
 export default App
