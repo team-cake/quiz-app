dbc0da8880dffa31087a9eab864a6b5135f9f869
team-cake <eka.cheuk@gmail.com>
2020-09-24 19:29:19 +0200

    set up corresponding props from the QuestionCard.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import QuestionCard from './components/QuestionCard'
+
+const TOTAL_QUESTIONS = 50
 
 function App() {
+	const [loading, setLoading] = useState(false)
+	const [questions, setQuestions] = useState([])
+	const [number, setNumber] = useState(0)
+	const [userAnswers, setUserAnswers] = useState([])
+	const [score, setScore] = useState(0)
+	const [gameOver, setGameOver] = useState(true)
+
 	const startTrivia = async () => {}
 
 	const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {}
@@ -15,6 +25,17 @@ function App() {
 			</button>
 			<p className='score'>Score:</p>
 			<p>Loading questions...</p>
+			<QuestionCard
+				questionNr={number + 1}
+				totalQuestions={TOTAL_QUESTIONS}
+				question={questions[number].question}
+				answers={questions[number].answers}
+				userAnswer={userAnswers ? userAnswers[number] : undefined}
+				callback={checkAnswer}
+			/>
+			<button className='next' onClick={nextQuestion}>
+				Next Question
+			</button>
 		</div>
 	)
 }
