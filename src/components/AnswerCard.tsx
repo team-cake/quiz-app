b71d60266d87acc94965ddf060efdec903d579f2
team-cake <eka.cheuk@gmail.com>
2020-09-28 22:14:30 +0200

    Divided the QuestionCard.tsx, so there is also an AnswerCard.tsx for styling purposes and reusability

---
+++ src/components/AnswerCard.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+// Types
+import { AnswerObject } from '../App'
+// Styles
+import { Button, Typography } from '@material-ui/core'
+
+type Props = {
+	answers: string[]
+	callback: (e: React.MouseEvent<HTMLButtonElement>) => void
+	userAnswer: AnswerObject | undefined
+}
+
+const AnswerCard: React.FC<Props> = ({ answers, callback, userAnswer }) => (
+	<div>
+		{answers.map((answer) => (
+			<div key={answer}>
+				<Button
+					variant='outlined'
+					color='primary'
+					disabled={userAnswer ? true : false}
+					value={answer}
+					onClick={callback}
+				>
+					<span dangerouslySetInnerHTML={{ __html: answer }} />
+				</Button>
+			</div>
+		))}
+	</div>
+)
+
+export default AnswerCard
--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,23 +1,16 @@
 import React from 'react'
-// Types
-import { AnswerObject } from '../App'
+
 // Styles
 import { Button, Typography } from '@material-ui/core'
 
 type Props = {
 	question: string
-	answers: string[]
-	callback: (e: React.MouseEvent<HTMLButtonElement>) => void
-	userAnswer: AnswerObject | undefined
 	questionNr: number
 	totalQuestions: number
 }
 
 const QuestionCard: React.FC<Props> = ({
 	question,
-	answers,
-	callback,
-	userAnswer,
 	questionNr,
 	totalQuestions,
 }) => (
@@ -26,19 +19,6 @@ const QuestionCard: React.FC<Props> = ({
 			Question: {questionNr} / {totalQuestions}
 		</Typography>
 		<Typography dangerouslySetInnerHTML={{ __html: question }} />
-		{answers.map((answer) => (
-			<div key={answer}>
-				<Button
-					variant='outlined'
-					color='primary'
-					disabled={userAnswer ? true : false}
-					value={answer}
-					onClick={callback}
-				>
-					<span dangerouslySetInnerHTML={{ __html: answer }} />
-				</Button>
-			</div>
-		))}
 	</div>
 )
 
